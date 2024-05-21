4c062f342629d6c66f747c25154dc7cbae1ae519
LuizinVS <168640684+LuizinVS@users.noreply.github.com>
2024-05-20 22:00:47 -0300

    Add files via upload

--- JavaScript/js/Exemplo05Cliente.js
+++ JavaScript/js/Exemplo05Cliente.js
@@ -7,6 +7,8 @@ const campoApelido = document.getElementById("apelido");
 const campoRazaoSocial = document.getElementById("razaoSocial");
 
 const clientes = JSON.parse(localStorage.getItem("clientes")) || [];
+let idProximoCliente = 0;
+let idAtual = -1;
 
 const campoCpf = document.getElementById('cpf');
 const mascaraCpf = { mask: "000.000.000-00"};
@@ -16,12 +18,12 @@ const campoCnpj = document.getElementById("cnpj");
 const mascaraCnpj = {mask : "00.000.000/0000-00"};
 const mascaraCampoCnpj = IMask(campoCnpj, mascaraCnpj);
 
-function apresentarCamposPj(){
+function apresentarCamposPJ(){
     apresentarCampos("campos-pj");
     removerCampos("campos-pf");
 }
 
-function apresentarCamposPf(){
+function apresentarCamposPF(){
     apresentarCampos("campos-pf");
     removerCampos("campos-pj")
 }
@@ -62,11 +64,47 @@ function salvarCliente(){
     // document.querySelector("input[name='tipoPessoa':checked]") irá retornar o primeiro input que contenha o name 'tipoPessoa' que está selecionado, neste cenário é o campo que o usuário escolheu.
     const tipoPessoa = document.querySelector("input[name= 'tipoPessoa']:checked").value;
     console.log(tipoPessoa);
+    if (idAtual == -1){
+    const cliente = cadastrarCliente(nome, cpf, dataNascimento, apelido, cnpj, razaoSocial, tipoPessoa);
+    //adicionando o cliente na lista de clientes
+    clientes.push(cliente);
+    } else{
+        const cliente = editarCliente(nome, cpf, dataNascimento, apelido, cnpj, razaoSocial, tipoPessoa, idAtual);
 
-    criarLinha(nome, cpf, dataNascimento, apelido, cnpj, razaoSocial, tipoPessoa);
+    }
+    //jso.strignify gera um texto a partir de um objeto de uma lista
+    const clientesString = JSON.stringify(clientes);
+    localStorage.setItem("clientes", clientesString);
+}
+
+function editarCliente(nome, cpf, dataNascimento, apelido, cnpj, razaoSocial, tipoPessoa, idAtual){
+    for(let i = 0; i < clientes.length; i++){
+        let cliente = clienstes[i];   
+        if (cliente.id == idAtual){
+            cliente.nome = nome;
+            cliente.cpf = cpf;
+            cliente.dataNascimento = dataNascimento;
+            cliente.apelido = apelido;
+            cliente.cnpj = cnpj;
+            cliente.razaoSocial = razaoSocial;
+            cliente.tipoPessoa = tipoPessoa;
+            limparCampos();
+            carregarClientesNaTabela();
+            idAtual = -1;
+            return cliente;
+        }
+     }
+
+}
+
+function cadastrarCliente(nome, cpf, dataNascimento, apelido, cnpj, razaoSocial, tipoPessoa) {
+    idProximoCliente += 1;
+
+    criarLinha(idProximoCliente, nome, cpf, dataNascimento, apelido, cnpj, razaoSocial, tipoPessoa);
     limparCampos();
 
     const cliente = {
+        id: idProximoCliente,
         nome: nome,
         cpf: cpf,
         dataNascimento: dataNascimento,
@@ -75,21 +113,17 @@ function salvarCliente(){
         razaoSocial: razaoSocial,
         tipoPessoa: tipoPessoa
     };
-    //adicionando o cliente na lista de clientes
-    clientes.push(cliente);
-    //jso.strignify gera um texto a partir de um objeto de uma lista
-    const clientesString = JSON.stringify(clientes);
-    localStorage.setItem("clientes", clientesString);
+    return cliente;
 }
 
-function criarLinha(nome, cpf, dataNascimento, apelido, cnpj, razaoSocial, tipoPessoa){
+function criarLinha(idCliente, nome, cpf, dataNascimento, apelido, cnpj, razaoSocial, tipoPessoa){
      // Criar um elemento de tr no javascript
      const novaLinha = document.createElement("tr");
     
      // Criar um elemento de coluna para o código
      const novaCelulaCodigo = document.createElement("td");
      // Definir o conteúdo dessa coluna do código
-     novaCelulaCodigo.innerHTML = "2";
+     novaCelulaCodigo.innerHTML = idCliente;
      
      // Criar um elemento de coluna para o nome
      const novaCelulaNome = document.createElement("td");
@@ -117,9 +151,13 @@ function criarLinha(nome, cpf, dataNascimento, apelido, cnpj, razaoSocial, tipoP
  
  novoBotaoEditar.appendChild(novoIconeEditar);
  novoBotaoEditar.innerHTML += " Editar";
+ novoBotaoEditar.addEventListener("click", editar);
+ novoBotaoEditar.setAttribute("data-id", idCliente);
  
  novoBotaoApagar.appendChild(novoIconeApagar);
  novoBotaoApagar.innerHTML += " Apagar";
+ novoBotaoApagar.addEventListener("click", apagar);
+ novoBotaoApagar.setAttribute("data-id", idCliente)
  
  novaCelulaBotoes.appendChild(novoBotaoEditar);
  novaCelulaBotoes.appendChild(novoBotaoApagar);
@@ -139,6 +177,59 @@ function criarLinha(nome, cpf, dataNascimento, apelido, cnpj, razaoSocial, tipoP
      tbodyDaTabela.appendChild(novaLinha);
 }
 
+function editar(event){
+    let target = event.target
+    //Obter o data atributo id do botao que foi realizado o click
+    let id = parseInt(target.getAttribute("data-id"));
+
+    //percorrer cada um dos elementos para editar 
+    for (let i = 0;i <  clientes.length; i++){
+        let cliente = clientes[i];
+        if (cliente.id == id) {
+            idAtual = cliente.id;   
+            campoNome.value = cliente.nome;
+            campoCpf.value = cliente.cpf;
+            campoDataNascimento.value = cliente.dataNascimento;
+            campoApelido.value = cliente.apelido;
+            campoCnpj.value = cliente.cnpj;
+            campoRazaoSocial.value = cliente.razaoSocial;
+            let evento = new Event("change");
+
+            if(cliente.tipoPessoa == "pf"){
+            campoTipoPF.checked = true;
+            campoTipoPF.dispatchEvent(evento);
+            }
+        else{
+            campoTipoPJ.checked = true;
+            campoTipoPJ.dispatchEvent(evento);
+        return;
+        }
+        }
+   
+    }
+}
+
+function apagar(event){
+    let target = event.target
+    //Obter o data atributo id do botao que foi realizado o click
+    let id = parseInt(target.getAttribute("data-id"));
+
+    //percorrer cada um dos elementos para remover o que possuir o id
+    for (let i = 0;i <  clientes.length; i++){
+        let cliente = clientes[i];
+        if (cliente.id == id) {
+        //remover o elemento que contenha o indice d alista de cliente
+        clientes.splice(i ,1);
+        let clientesString = JSON.stringify(clientes);
+        //atualizar a lista de clientes no local storage 
+        localStorage.setItem("clientes", clientesString);
+        carregarClientesNaTabela();
+        return;
+        }
+   
+    }
+}
+
 function limparCampos(){
     campoNome.value = "";
     campoCpf.value = "";
@@ -150,9 +241,12 @@ function limparCampos(){
 }
 
 function carregarClientesNaTabela(){
+    var tbody = document.querySelector("table tbody");
+    tbody.innerHTML = "";
     for(let i = 0; i < clientes.length; i++){
         let cliente = clientes[i];
         criarLinha(
+            cliente.id,
             cliente.nome,
             cliente.cpf,
             cliente.dataNascimento,
@@ -160,13 +254,26 @@ function carregarClientesNaTabela(){
             cliente.cnpj,
             cliente.razaoSocial,
             cliente.tipoPessoa )
+
+            if (cliente.id > idProximoCliente)
+                idProximoCliente = cliente.id;
+            
+    }
+}
+
+function alterarTipoPessoa(){
+    if (campoTipoPF.checked === true){
+        apresentarCamposPF();
+    }
+    else if (campoTipoPJ.checked === true){
+        apresentarCamposPJ();
     }
 }
 
 carregarClientesNaTabela();
 
-campoTipoPJ.onclick = apresentarCamposPj;
-campoTipoPF.onclick = apresentarCamposPf;
+campoTipoPJ.onchange = apresentarCamposPJ;
+campoTipoPF.onchange = apresentarCamposPF;
 cadastrar.onclick = salvarCliente;
 
 // Local Storage: é um local que permite armazenar dados sem tempo de expiração
